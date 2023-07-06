53af1dbcd01815efe4123388ad53394cdfb9e571
Moorthy M Nair <83420459+moorthynair@users.noreply.github.com>
2023-07-06 16:51:49 +0530

    Update health.py

--- health.py
+++ health.py
@@ -11,5 +11,5 @@ import numpy as np
 
 def health_impact(AQI_category):
     sheet = pd.read_excel('inputs/AQI_breakpoint.xlsx', sheet_name='AQI_IND')
-    impact = sheet.loc[sheet['Category']=='Moderate', 'Health Impacts'].reset_index(drop=True)
+    impact = sheet.loc[sheet['Category']==AQI_category, 'Health Impacts'].reset_index(drop=True)
     return impact[0]
